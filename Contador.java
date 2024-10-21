b3987952158a6521c0b3833085d0758eba2d0cd6
Richard765 <richardexposito04@gmail.com>
2024-10-21 20:27:17 +0200

    H

--- Contador.java
+++
@@ -1,7 +0,0 @@
-public class Contador {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
-            System.out.println("Número: " + i);
-        }
-    }
-}
--- Contador/Contador.java
+++ Contador/Contador.java
@@ -1,5 +1,6 @@
 public class Contador {
     public static void main(String[] args) {
+<<<<<<< HEAD
 <<<<<<< HEAD:Contador/Contador.java
 <<<<<<< HEAD:Contador.java
         for (int i = 1; i <= 30; i++) {
@@ -9,6 +10,9 @@ public class Contador {
 =======
         for (int i = 1; i <= 20; i++) {
 >>>>>>> parent of 0767f35 (Tercer commit):Contador.java
+=======
+        for (int i = 1; i <= 10; i++) {
+>>>>>>> parent of f790d50 (Segundo commit)
             System.out.println("Número: " + i);
         }
     }
