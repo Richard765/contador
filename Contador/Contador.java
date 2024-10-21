aa7867c771e4e61846abb55f3b27b3bdf4399784
Richard765 <richardexposito04@gmail.com>
2024-10-21 20:20:49 +0200

    Volver hacia atras

--- Contador/Contador.java
+++ Contador/Contador.java
@@ -1,6 +1,10 @@
 public class Contador {
     public static void main(String[] args) {
+<<<<<<< HEAD:Contador.java
         for (int i = 1; i <= 30; i++) {
+=======
+        for (int i = 1; i <= 10; i++) {
+>>>>>>> parent of f790d50 (Segundo commit):Contador/Contador.java
             System.out.println("Número: " + i);
         }
     }
